0281a6660381b7c618f78e45d3cfe6fd868d4738
DESKTOP-P40HFGF\User <alexrusakovich1@gmail.com>
2026-01-12 12:28:22 +0300

    Implemented base features: Adding/removing points, copy to/from clipboard (Excel columns format), removing all values.

--- Functions/ViewModels/FunctionPointsListViewModel.cs
+++ Functions/ViewModels/FunctionPointsListViewModel.cs
@@ -1,14 +1,82 @@
 ﻿using Caliburn.Micro;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Functions.ViewModels;
 
-public class FunctionPointsListViewModel : Screen
+public class FunctionPointsListViewModel : PropertyChangedBase
 {
     public ObservableCollection<FunctionPointViewModel> FunctionPoints { get; set; } = [];
+    public event EventHandler<ObservableCollection<FunctionPointViewModel>> FunctionPointsDataChanged;
 
-    public void CreateNewPoint()
+    public FunctionPointsListViewModel()
     {
-        FunctionPoints.Add(new FunctionPointViewModel());
+        FunctionPoints.CollectionChanged += (o, e) => TriggreFunctionPointsDataChangedEvent();
+    }
+
+    public void CreateNewPoint() => AddPoint(new());
+
+    public void AddPoint(FunctionPointViewModel point)
+    {
+        point.PropertyChanged += (o, e) => TriggreFunctionPointsDataChangedEvent();
+        FunctionPoints.Add(point);
+    }
+
+    public void RemovePoint(FunctionPointViewModel pointToRemove)
+    {
+        FunctionPoints.Remove(pointToRemove);
+        pointToRemove.PropertyChanged -= (o, e) => TriggreFunctionPointsDataChangedEvent();
+    }
+
+    public void RemoveAllPoints()
+    {
+        foreach (var functionPoint in FunctionPoints)
+        {
+            functionPoint.PropertyChanged -= (o, e) => TriggreFunctionPointsDataChangedEvent();
+        }
+
+        FunctionPoints.Clear();
+    }
+
+    public void CopyToClipboard()
+    {
+        Clipboard.SetText(string.Join("\r\n", FunctionPoints.Select(point => $"{point.X}\t{point.Y}")));
+    }
+
+    public void PasteFromClipboard()
+    {
+        if (!Clipboard.ContainsText())
+            return;
+
+        foreach (var functionPoint in FunctionPoints)
+        {
+            RemovePoint(functionPoint);
+        }
+
+        var clipboardText = Clipboard.GetText();
+        var pointTexts = clipboardText.Split("\r\n");
+        var points = pointTexts
+            .Select(pointText =>
+            {
+                var splittedValues = pointText.Split("\t");
+                if (splittedValues.Length != 2)
+                    return null;
+
+                return double.TryParse(splittedValues[0], out var x)
+                    && double.TryParse(splittedValues[1], out var y)
+                    ? new FunctionPointViewModel { X = x, Y = y }
+                    : null;
+            })
+            .Where(x => x != null);
+
+        foreach (var point in points)
+        {
+            AddPoint(point!);
+        }
+    }
+
+    private void TriggreFunctionPointsDataChangedEvent()
+    {
+        FunctionPointsDataChanged?.Invoke(this, FunctionPoints);
     }
 }
--- Functions/ViewModels/FunctionsDashboardViewModel.cs
+++ Functions/ViewModels/FunctionsDashboardViewModel.cs
@@ -1,8 +1,30 @@
 ﻿using Caliburn.Micro;
+using ScottPlot;
+using ScottPlot.WPF;
+using System.Collections.ObjectModel;
 
 namespace Functions.ViewModels;
 
 public class FunctionsDashboardViewModel : Screen
 {
     public FunctionPointsListViewModel FunctionPointsListViewModel { get; set; } = new();
+    public WpfPlot PlotControl { get; private set; } = new WpfPlot();
+
+    public FunctionsDashboardViewModel()
+    {
+        FunctionPointsListViewModel.FunctionPointsDataChanged += OnFunctionPointsDataChanged;
+        var plot = PlotControl.Multiplot.GetPlot(0);
+        plot.XLabel("Абсолютная отметка [мм]");
+        plot.YLabel("Температура [°C]");
+    }
+
+    private void OnFunctionPointsDataChanged(object? sender, ObservableCollection<FunctionPointViewModel> e)
+    {
+        var plot = PlotControl.Multiplot.GetPlot(0);
+        var xs = e.Select(x => x.X).ToArray();
+        var ys = e.Select(x => x.Y).ToArray();
+        plot.Clear();
+        plot.Add.ScatterLine(xs, ys);
+        PlotControl.Refresh();
+    }
 }
